739e9183ca002a92c8a1fd4a3561ed2613d8a145
RaY8118 <parthghadi43@gmail.com>
2025-03-17 15:07:07 +0530

    Initial Commit

---
+++ app.js
@@ -0,0 +1,32 @@
+import express from 'express';
+import { PORT } from './config/env.js';
+import userRouter from './routes/user.routes.js';
+import authRouter from './routes/auth.routes.js';
+import subscriptionRouter from './routes/subscription.routes.js';
+import connectToDatabase from './database/mongodb.js';
+import errorMiddleware from './middlewares/error.middleware.js';
+import cookieParser from 'cookie-parser';
+import arcjectMiddleware from './middlewares/arcjet.middleware.js';
+
+const app = express()
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+/* app.use(arcjectMiddleware); */
+
+app.use('/api/v1/auth', authRouter);
+app.use('/api/v1/users', userRouter);
+app.use('/api/v1/subscriptions', subscriptionRouter);
+
+app.use(errorMiddleware);
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the subscription tracker API!')
+})
+app.listen(PORT, async () => {
+  console.log(`Subscription Tracker API is running on https://localhost:${PORT}`)
+  await connectToDatabase()
+})
+
+export default app
---
+++ eslint.config.js
@@ -0,0 +1,8 @@
+import pluginJs from "@eslint/js";
+
+
+/** @type {import('eslint').Linter.Config[]} */
+export default [
+  {files: ["**/*.js"], languageOptions: {sourceType: "script"}},
+  pluginJs.configs.recommended,
+];
